94a669ef503853374e6f5a91fe1f42ef60cd4de5
rossant <rossant@github>
2013-03-25 20:15:07 +0000

    added comments to examples in chapter 2

--- chapter2/center.py
+++ chapter2/center.py
@@ -1,5 +1,5 @@
 import networkx as nx
-g = nx.read_edgelist('0.edges')
+g = nx.read_edgelist('data/facebook/0.edges')
 sg = nx.connected_component_subgraphs(g)[0]
 center = [node for node in sg.nodes() if nx.eccentricity(sg, node) == nx.radius(sg)]
 print(center)
